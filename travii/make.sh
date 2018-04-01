77940e29911e1e1413909d2e0cc49f0aaa4f81b8
Timardo <33033989+Timardo@users.noreply.github.com>
2018-04-02 00:07:35 +0200

    Playing with Travis CI
    :party:

--- travii/make.sh
+++ travii/make.sh
@@ -29,11 +29,6 @@ cd forge
 git checkout c6aa04325e8a80af88755adb2339a402e7e90ebb
 cd ..
 git pull origin master
-./gradlew --parallel -PofficialBuild clean setupCauldron --parallel jar packageBundle
+./gradlew setupCauldron jar
 cp build/distributions/Thermos*server* $HOME/release/Thermos-1.7.10-1614-server.jar
 cd build/distributions/
-unzip Thermos*bundle*zip
-rm -r bin/unknown
-mv bin libraries
-zip -r libraries.zip libraries
-cp libraries.zip $HOME/release
